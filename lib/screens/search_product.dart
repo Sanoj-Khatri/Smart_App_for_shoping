670f3c9e386165798c334ed561a02d1c4a85d8a2
Muhammad Shahzad <108967038+Shahzad-Official@users.noreply.github.com>
2022-11-07 22:17:30 +0500

    updated

--- lib/screens/search_product.dart
+++ lib/screens/search_product.dart
@@ -27,100 +27,91 @@ class _SearchProductState extends State<SearchProduct> {
     'B_Item3',
     'B_Item4',
   ];
-  List<String> emptylist = [];
+  List<String> emptylistCompany = [];
+  List<String> emptylistItem = [];
 
   String? selectedValueCompany;
-  //final TextEditingController textEditingController = TextEditingController();
 
   String? selectedValueItem;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            DropdownButtonFormField2(
-              value: selectedValueCompany,
-              items: companyList
-                  .map(
-                    (e) => DropdownMenuItem(
-                      child: Text(e),
-                      value: e,
-                    ),
-                  )
-                  .toList(),
-              onChanged: (val) {
-                // setState(() {
-                //   selectedValueCompany = val as String;
-                // });
-
-                //=============================================>>>Items Disable after selection
-                if (!emptylist.contains(val)) {
-                  setState(() {
-                    selectedValueCompany = val! as String?;
-                    emptylist.add(selectedValueCompany!);
-                  });
-                }
-                //======
-              },
-              icon: const Icon(Icons.arrow_drop_down),
-              decoration: const InputDecoration(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              DropdownButtonFormField2(
+                value: selectedValueCompany,
+                items: companyList
+                    .map(
+                      (e) => DropdownMenuItem(
+                        child: Text(e),
+                        value: e,
+                      ),
+                    )
+                    .toList(),
+                onChanged: (val) {
+                  setState(
+                    () {
+                      selectedValueCompany = val! as String?;
+                      emptylistCompany.add(selectedValueCompany!);
+                    },
+                  );
+                },
+                icon: const Icon(Icons.arrow_drop_down),
+                decoration: const InputDecoration(
                   labelText: "Select Mart",
                   prefixIcon: Icon(Icons.shopping_bag),
-                  border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            DropdownButtonFormField2(
-              value: selectedValueItem,
-              items: items
-                  .map(
-                    (e) => DropdownMenuItem(
-                      child: Text(e),
-                      value: e,
-                    ),
-                  )
-                  .toList(),
-              onChanged: (val) {
-                if (!emptylist.contains(val)) {
-                  setState(() {
-                    selectedValueItem = val! as String?;
-                    emptylist.add(selectedValueItem!);
-                  });
-                }
-
-                // setState(() {
-                //   selectedValueItem = val as String;
-                // });
-              },
-              icon: const Icon(Icons.arrow_drop_down),
-              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              DropdownButtonFormField2(
+                value: selectedValueItem,
+                items: items
+                    .map(
+                      (e) => DropdownMenuItem(
+                        child: Text(e),
+                        value: e,
+                      ),
+                    )
+                    .toList(),
+                onChanged: (val) {
+                  setState(
+                    () {
+                      selectedValueItem = val! as String?;
+                      emptylistItem.add(selectedValueItem!);
+                    },
+                  );
+                },
+                icon: const Icon(Icons.arrow_drop_down),
+                decoration: const InputDecoration(
                   labelText: "Select Company",
                   prefixIcon: Icon(Icons.branding_watermark),
-                  border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Divider(
-              thickness: 2,
-              color: Color.fromARGB(124, 155, 39, 176),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 25,
-            ),
-            ClipRRect(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Divider(
+                thickness: 2,
+                color: Color.fromARGB(124, 155, 39, 176),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 25,
+              ),
+              ClipRRect(
                 borderRadius: BorderRadius.circular(20),
                 child: Container(
                   width: MediaQuery.of(context).size.width / 1.1,
-                  height: MediaQuery.of(context).size.height / 4,
                   decoration: BoxDecoration(
                     gradient: const LinearGradient(
                       colors: [
@@ -134,17 +125,27 @@ class _SearchProductState extends State<SearchProduct> {
                     borderRadius: BorderRadius.circular(20),
                     shape: BoxShape.rectangle,
                   ),
-                  child: ListTile(
-                    title: Text(
-                      emptylist.toString(),
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                    subtitle: Text(emptylist.toString()),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: emptylistItem.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          emptylistCompany[index].toString(),
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                        subtitle: Text(
+                          emptylistItem[index].toString(),
+                        ),
+                      );
+                    },
                   ),
-                )),
-          ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
